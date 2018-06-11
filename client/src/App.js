69ee5027c5341041773823c7bd3d086e536a820e
Mark Perry <mark.j.perry11@gmail.com>
2018-06-10 22:23:08 -0400

    Add landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Header from "./components/layout/Header";
+import Landing from "./components/layout/Landing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CssBaseline />
         <Header />
+        <Landing />
       </div>
     );
   }
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default props => {
+  return (
+    <div className="landing">
+      <div className="attribution">
+        <Typography id="attribution">
+          Photo by Thought Catalog on Unsplash
+        </Typography>
+      </div>
+    </div>
+  );
+};
