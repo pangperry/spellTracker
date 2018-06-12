921724709a8188043a800ea25f615a7bd2f13f0b
Mark Perry <mark.j.perry11@gmail.com>
2018-06-12 17:14:31 -0400

    Add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import "./App.css";
 import Header from "./components/layout/Header";
 import Navbar from "./components/layout/Navbar";
@@ -8,19 +10,22 @@ import Landing from "./components/layout/Landing";
 import Main from "./components/Main";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <CssBaseline />
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <CssBaseline />
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isAuthenticated: false,
+  teacher: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+    // case SET_CURRENT_USER:
+  }
+}
